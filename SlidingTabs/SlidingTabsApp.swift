47c5234b89f7eb9804d6f9aec39f831d12bc5ee6
Tamer Afyon <203502050@ogr.altinbas.edu.tr>
2024-04-28 02:04:15 +0300

    Initial Commit

---
+++ SlidingTabs/SlidingTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingTabsApp.swift
+//  SlidingTabs
+//
+//  Created by Tamer Afyon on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SlidingTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
